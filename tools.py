cbef703b753b468b6891c03c995237694b5a90a0
Daniel Vasu <dnv@192-168-0-231.rdsnet.ro>
2023-09-23 20:59:54 +0300

    added a new input argument for gui

--- tools.py
+++ tools.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
-import os
-import tkinter as tk
-from tkinter import filedialog as fd
+import os, sys
 
 class RetVal():
     OK = 0
@@ -27,42 +25,15 @@ class Tools:
     
     def parse_inputs():
         parser = argparse.ArgumentParser()
-        parser.add_argument('-i', '--input', type = Tools.dir_path, required = True, help = "Input directory containing ppt/pptx files")
-        parser.add_argument('-o', '--output', type = Tools.dir_path, required = True, help = "Output directory for the converted files")
+        parser.add_argument('-g', '--gui', action='store_true', default= False, help = "Start the program in GUI mode")
+        parser.add_argument('-i', '--input',  type = Tools.dir_path, help = "Input directory containing ppt/pptx files")
+        parser.add_argument('-o', '--output', type = Tools.dir_path, help = "Output directory for the converted files")
         args = parser.parse_args()
 
-        return args.input, args.output
-    
+        #reject i/o if gui is rendered
+        if args.gui == False:
+            if args.input == None or args.output == None:
+                print("ERROR: input -i DIR and output -o DIR args. are mandatoy in non-GUI mode!")
+                sys.exit()
 
-class Gui:
-    def render(self):
-        window = tk.Tk()
-        window.title(f"{Tools.APP_NAME}     v{Tools.APP_VERSION}")
-        window.resizable(False, False)
-        window.geometry('560x480')
-
-
-        label1 = tk.Label(text="Select the input directory containing PPT/PPTX files").place(x=5, y=20)
-        self.input1 = tk.Entry(fg="white", bg="gray", width=50)
-        self.input1.place(x=5, y=50)
-        btn1 = tk.Button(text="Browse",command= lambda: self.GetFilePath(self.input1)).place(x=470, y=48)
-        
-        label2 = tk.Label(text="Select the output directory").place(x=5, y=100)
-        self.input2 = tk.Entry(fg="white", bg="gray", width=50)
-        self.input2.place(x=5, y=130)
-        btn2 = tk.Button(text="Browse", command= lambda: self.GetFilePath(self.input2)).place(x=470, y=128)
- 
-        window.mainloop()
-
-
-    def ClearInputs(self):
-        self.input_path.delete(0, tk.END)
-        self.output_path.delete(0, tk.END)
-
-
-    def GetFilePath(self, inputX):
-        path = fd.askdirectory()
-        inputX.delete(0, tk.END)
-        inputX.insert(0,path)
-
-    
+        return args.input, args.output, args.gui
